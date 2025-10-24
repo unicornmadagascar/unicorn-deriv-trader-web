24e18cd5a76b7218a54c38f9bc4ef5affd8d5161
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2025-10-25 02:19:23 +0300

    Update web version

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const WS_URL = `wss://ws.derivws.com/websockets/v3?app_id=105747`;
     
     // Votre token comme constante
-    const API_TOKEN = "VOTRE_API_TOKEN_ICI";
+    const API_TOKEN = "wgf8TFDsJ8Ecvze";
 
     const connectBtn = document.getElementById("connectBtn");
     const statusSpan = document.getElementById("status");
