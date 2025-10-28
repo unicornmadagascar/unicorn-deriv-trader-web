e8a2a93bb67ca6882ebf4ba9b05b9fd9e08e86bb
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2025-10-28 17:05:47 +0300

    Update web version

--- app.js
+++ app.js
@@ -507,13 +507,13 @@ document.addEventListener("DOMContentLoaded", () => {
      // Si le socket est déjà ouvert, on le réutilise
      if (ws && ws.readyState === WebSocket.OPEN) {
        console.log("♻️ WebSocket déjà connecté, réutilisation...");
-       return ws;
+       return;
      }
 
      // Si le socket est en cours d’ouverture, attendre
      if (ws && ws.readyState === WebSocket.CONNECTING) {
        console.log("⏳ WebSocket en cours d’ouverture...");
-       return ws;
+       return;
      }
 
     ws = new WebSocket(WS_URL);
