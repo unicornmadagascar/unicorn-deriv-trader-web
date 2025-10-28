5c8a502c40c9853de599ff30456df9f8306d7b39
unicornmadagascar <unicorn.madagascar.electron@outlook.com>
2025-10-28 14:42:14 +0300

    Update web version

--- app.js
+++ app.js
@@ -240,7 +240,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   }
 
-  function startAutomation(ws2) {
+  function startAutomation(ws) {
     console.log("Connecting...");
 
     if (currentSymbol === null) {
@@ -249,18 +249,18 @@ document.addEventListener("DOMContentLoaded", () => {
       return;
     }
 
-    ws2.onopen = () => {
+    ws.onopen = () => {
       console.log("✅ Connecté au WebSocket Deriv");
-      ws2.send(JSON.stringify({ authorize: TOKEN }));
+      ws.send(JSON.stringify({ authorize: TOKEN }));
     };
 
-    ws2.onmessage = (msg) => {
+    ws.onmessage = (msg) => {
       const data = JSON.parse(msg.data);
 
       // Autorisation réussie → abonnement aux ticks
       if (data.authorize) {
          console.log("🔑 Autorisé, abonnement aux ticks...");
-         ws2.send(JSON.stringify({ ticks: currentSymbol, subscribe: 1 }));
+         ws.send(JSON.stringify({ ticks: currentSymbol, subscribe: 1 }));
       }
 
       // Quand un tick arrive
@@ -307,11 +307,11 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   }
 
-  function stopAutomation(ws2) {
-    if (ws2 && (ws2.readyState === WebSocket.CONNECTING || ws2.readyState === WebSocket.OPEN)) {
+  function stopAutomation(ws) {
+    if (ws && (ws2.readyState === WebSocket.CONNECTING || ws2.readyState === WebSocket.OPEN)) {
        // Envoyer unsubscribe avant de fermer
-       ws2.send(JSON.stringify({ forget_all: "ticks" }));
-       ws2.close();
+       ws.send(JSON.stringify({ forget_all: "ticks" }));
+       ws.close();
     }
   }
 
@@ -796,15 +796,15 @@ closeAll.onclick=()=>{
   const toggleAutomationBtn = document.getElementById("toggleAutomation");
   toggleAutomationBtn.addEventListener("click", () => {
     automationRunning = !automationRunning;
-    let ws2 = new WebSocket(WS_URL);
+    let ws = new WebSocket(WS_URL);
     if (automationRunning) {
       toggleAutomationBtn.textContent = "Stop Automation";
       toggleAutomationBtn.style.background = "linear-gradient(90deg,#f44336,#e57373)";
-      startAutomation(ws2);
+      startAutomation(ws);
     } else {
       toggleAutomationBtn.textContent = "Launch Automation";
       toggleAutomationBtn.style.background = "linear-gradient(90deg,#4caf50,#81c784)";
-      stopAutomation(ws2);
+      stopAutomation(ws);
     }
   });
 
